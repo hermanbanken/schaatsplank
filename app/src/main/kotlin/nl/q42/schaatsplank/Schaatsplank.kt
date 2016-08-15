cf872962cdc86e6057a3aeef2f34b19176d762d8
Herman Banken <hermanbanken@gmail.com>
2016-08-15 22:03:46 +0200

    Create service

--- app/src/main/kotlin/nl/q42/schaatsplank/Schaatsplank.kt
+++ app/src/main/kotlin/nl/q42/schaatsplank/Schaatsplank.kt
@@ -16,6 +16,7 @@ import android.view.View.OnTouchListener
 import fi.iki.elonen.NanoHTTPD
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.contentView
+import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.subjects.BehaviorSubject
 import timber.log.Timber
@@ -28,109 +29,29 @@ import java.net.ServerSocket
 /**
  * @author Herman Banken, Q42
  */
-class Schaatsplank: Activity(), SensorEventListener, OnTouchListener {
-    private var sensorManager: SensorManager? = null
-    private var gravSensor: Sensor? = null
-    private var acclSensor: Sensor? = null
-    private var httpServer: HttpServer? = null
-    private var socketServer: SocketServer? = null
+class Schaatsplank: Activity() {
 
-    private val acceleration: BehaviorSubject<SensorEvent> = BehaviorSubject.create()
-    private val gravity: BehaviorSubject<SensorEvent> = BehaviorSubject.create()
-    private val finger: BehaviorSubject<Float> = BehaviorSubject.create()
+    var disposable: Subscription? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        acclSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
-        gravSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
+
     }
 
     override fun onResume() {
         super.onResume()
-
-        val wifiMgr = getSystemService(WIFI_SERVICE) as WifiManager
-        val wifiInfo = wifiMgr.connectionInfo
-        val ip = wifiInfo.ipAddressString()
-
-        Timber.i("SensorManager $sensorManager")
-        Timber.i("Sensor $gravSensor")
-        sensorManager?.registerListener(this, gravSensor, SensorManager.SENSOR_DELAY_GAME)
-        sensorManager?.registerListener(this, acclSensor, SensorManager.SENSOR_DELAY_GAME)
-        contentView?.setOnTouchListener(this)
-
-        Thread {
-            run {
-                for (port in 8080..8090) {
-                    Log.i(javaClass.simpleName, "HTTP: Trying port $ip:$port")
-                    if(available(port, ip)) {
-                        Log.i(javaClass.simpleName, "HTTP: Selected $ip:$port")
-                        runOnUiThread {
-                            textView.text = "Schaatsplank. Surf to http://$ip:$port/"
-                        }
-                        httpServer = HttpServer(ip, port, this)
-                        httpServer?.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false)
-                        break
-                    }
-                }
-                if(httpServer == null) {
-                    textView.text = "Cant open ports 8080-8090. App might be started multiple times. Please close all instances."
-                }
-                for (port in 8080..8090) {
-                    Log.i(javaClass.simpleName, "WS: Trying port $ip:$port")
-                    if(available(port, ip)) {
-                        Log.i(javaClass.simpleName, "WS: Selected $ip:$port")
-                        socketServer = SocketServer(
-                                this,
-                                acceleration,
-                                gravity,
-                                finger,
-                                port, ip)
-                        socketServer?.start()
-                        break
-                    }
-                }
-                if(socketServer == null) {
-                    runOnUiThread {
-                        textView.text = "Cant open ws ports 8080-8090. App might be started multiple times. Please close all instances."
-                    }
-                }
-            }
-        }.start()
+        val instance = SchaatsplankService.instance
+        if(instance != null) {
+            disposable = instance.logs
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { textView.text = it }
+        }
     }
 
     override fun onPause() {
-        socketServer?.stop()
-        httpServer?.stop()
+        disposable?.unsubscribe()
         super.onPause()
     }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        data.text = "Accuracy changed $accuracy"
-        Timber.i("Accuracy changed $accuracy")
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(event?.sensor == gravSensor) {
-            gravity.onNext(event)
-        }
-        if(event?.sensor == acclSensor) {
-            acceleration.onNext(event)
-        }
-    }
-
-    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
-        if(event?.action == MotionEvent.ACTION_DOWN || event?.action == MotionEvent.ACTION_MOVE) {
-            val x = event?.x?.toFloat()
-            if (x != null && view != null) {
-                finger.onNext(x / view.width)
-                return true
-            }
-        }
-        return false
-    }
-
 }
 
 /**
